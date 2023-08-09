74d4416ee3247c9619bc7ffecf025d06d8d0d0ef
dayCod <alayarahman07@gmail.com>
2023-08-09 17:24:48 +0700

    Update: Terminating Middleware Connect to DB and Log

--- app/Http/Middleware/TerminatingMiddleware.php
+++ app/Http/Middleware/TerminatingMiddleware.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\AppHttpRequest;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class TerminatingMiddleware
 {
@@ -17,5 +20,27 @@ class TerminatingMiddleware
     public function handle(Request $request, Closure $next)
     {
         return $next($request);
+    }
+
+    /**
+     * handle tasks after response has been sent to the browser
+     *
+     * @return void
+     */
+    public function terminate(Request $request, Response $response): void
+    {
+        $http_request = new AppHttpRequest([
+            'session_id'  => session()->getId(),
+            'user_id'     => $request->user()->id ?? null,
+            'ip'          => $request->ip(),
+            'ajax'        => $request->ajax(),
+            'url'         => $request->fullUrl(),
+            'payload'     => $request->toArray(),
+            'status_code' => $response->getStatusCode()
+        ]);
+
+        Log::info('http_request', $http_request->toArray());
+
+        $http_request->save();
     }
 }
