3d46b3c2f4e50bb2b33ea547d1c403212403d039
dayCod <alayarahman07@gmail.com>
2023-08-09 17:15:23 +0700

    Update: Register Middleware To Kernel

--- app/Models/AppHttpRequest.php
+++ app/Models/AppHttpRequest.php
@@ -4,10 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AppHttpRequest extends Model
 {
     use HasFactory;
 
+    /**
+     * Fill the model with an array of attributes.
+     *
+     * @var array
+     */
     protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'payload' => 'collection',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
